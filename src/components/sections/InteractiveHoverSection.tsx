369fa5a79cdf2c2ac175b9bc057ce3aa676c7aae
Marlon Ridge Bulaquena <digitalsoftwarestrategies@gmail.com>
2025-08-03 10:45:12 +0800

    g
    g

--- src/components/sections/InteractiveHoverSection.tsx
+++ src/components/sections/InteractiveHoverSection.tsx
@@ -6,9 +6,9 @@ const InteractiveHoverComponent: React.FC = () => {
     <div className="relative group w-full">
       {/* Wrapper div containing the icon and panels */}
       <div className="relative flex flex-col items-center w-full">
-        {/* Top panel - slides up from icon line */}
-        <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-0 opacity-0 translate-y-0 transition-all duration-700 ease-out group-hover:opacity-100 group-hover:-translate-y-4 bg-white rounded-lg shadow-lg p-4 w-full z-10">
-          <div className="bg-gray-200 rounded-md h-32 mb-3 flex items-center justify-center">
+        {/* Top panel - slides up from below icon */}
+        <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-0 opacity-0 translate-y-0 transition-all duration-700 ease-out group-hover:opacity-100 group-hover:-translate-y-8 bg-gradient-to-br from-[#c1c8d1] to-[#fefffb] rounded-tl-[20px] rounded-tr-[20px] border border-gray-300 p-4 w-full z-10 shadow-lg">
+          <div className="bg-white/50 rounded-md h-32 mb-3 flex items-center justify-center">
             <img 
               src={dividerIcon} 
               alt="Placeholder" 
@@ -26,8 +26,8 @@ const InteractiveHoverComponent: React.FC = () => {
           />
         </div>
 
-        {/* Bottom panel - slides down from icon line */}
-        <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-0 opacity-0 translate-y-0 transition-all duration-700 ease-out group-hover:opacity-100 group-hover:translate-y-4 bg-white rounded-lg shadow-lg p-4 w-full z-10">
+        {/* Bottom panel - slides down from below icon */}
+        <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-0 opacity-0 translate-y-0 transition-all duration-700 ease-out group-hover:opacity-100 group-hover:translate-y-8 bg-gradient-to-br from-[#a9b1bd] to-[#dbe4e3] rounded-bl-[20px] rounded-br-[20px] border border-gray-300 p-4 w-full z-10 shadow-lg">
           <p className="text-gray-700 text-sm mb-3">
             Discover our comprehensive food safety solutions and expert guidance for your business.
           </p>
